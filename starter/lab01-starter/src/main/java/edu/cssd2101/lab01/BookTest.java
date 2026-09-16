0c8be3a0681b8517b41d0d31485dd540ea7232b1
fati-hussein <sampilfatoumata@yahoo.com>
2026-09-16 14:42:22 -0400

    new class

---
+++ starter/lab01-starter/src/main/java/edu/cssd2101/lab01/BookTest.java
@@ -0,0 +1,11 @@
+package edu.cssd2101.lab01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+@Test
+void normalizesIsbn(){
+
+    assertTrue()
+    assertFalse()
+}
